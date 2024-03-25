f3f95511476a4a8194665cef2cbf2e89aa3997da
Kerolous Amged <kerolous.amged@nagwa.com>
2024-03-25 16:54:35 +0200

    feat: loaded csv

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+const DEFAULT_PROBLEMSET = "problems"
+
 func main() {
+
+	problemSet := DEFAULT_PROBLEMSET
+
+	if(len(os.Args) > 1){
+		problemSet = os.Args[1]
+	}
+
+	file , err := os.Open(fmt.Sprintf("questions-repo/%v.csv" , problemSet))
+
+	if err != nil {
+		log.Fatal("No File found")
+	}
+
+	reader := csv.NewReader(file)
 	
+	  
+	for {
+		data , err := reader.Read()
+		if (err == io.EOF){
+			break
+		}
+		fmt.Println(data)
+
+	}
+
+	defer file.Close()
+
+
 }
